97961e896f0267ede6cb6590831f2f7cb6fc639b
Your Name <you@example.com>
2020-12-17 10:46:33 -0500

    new files

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,49 @@
+// Create a copy of the data
+var tableData = data;
+
+function showTable(tableData){
+  var tbody = d3.select(".table").select("tbody");
+  tbody.html("");
+  tableData.forEach((x) => {
+    var row = tbody.append("tr");
+    Object.entries(x).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+};
+
+//write table to html
+showTable(tableData);
+
+var filterobject = {};
+
+function filterinput(){
+  var changedelement = d3.select(this).select("input");
+  var elementvalue = changedelement.property("value");
+  var filterid = changedelement.attr("id");
+  if (elementvalue){
+    filterobject[filterid] = elementvalue;
+  }
+  else {
+    delete filterobject[filterid];
+  }
+multifilter();
+}
+
+function multifilter(){
+  var filterdata = tableData;
+  Object.entries(filterobject).forEach(([key, value]) => {
+  filterdata = filterdata.filter(rowobject => rowobject[key] === value);
+  });
+
+  console.log(filterdata);
+  
+  //write filtered table to html
+  showTable(filterdata);
+};
+
+
+d3.selectAll(".filter").on("change", filterinput);
+
+
